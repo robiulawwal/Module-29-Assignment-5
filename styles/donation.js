b3d688a273225ffe20b20ce758d214179604b0de
robiulawwal <robiulawwal086@gmail.com>
2024-12-08 20:37:18 +0600

    the final output

--- styles/donation.js
+++ styles/donation.js
@@ -52,6 +52,7 @@ for (const card of mainSection) {
         const dontaedStoreAmmount = parseFloat(card.children[1].children[0].children[1].innerText);
         const historySec = document.getElementById('history-section');
         if (!isNaN(inputValue) && typeof inputValue === 'number' && inputValue >= 1 && inputValue <= myBalance) {
+            console.log(inputValue)
             if (myBalance <= 0) {
                 alert('insupicient balance')
                 return
